2e2361fd8a150f58d21ceedfb0045e4ff7670f9f
Govind Diwakar <sgf@sourcegoodfood.com>
2025-04-29 02:16:18 +0000

    fix: startup

--- packages/test-server/start-dev.sh
+++ packages/test-server/start-dev.sh
@@ -3,7 +3,13 @@
 		#Run migrations to ensure the database is updated
 		#cd packages/test-server
 		ls -l
-	    npx medusa db:migrate && yarn run build &&  && npx medusa develop --verbose -H ::0 -p 9000
-		 
+	    npx medusa db:migrate && yarn run build && npx medusa exec ./src/scripts/seed.ts && npx medusa develop --verbose -H ::0 -p 9000
+		RESULT=$?
+		if [ $RESULT -eq 0 ]; then
+		echo success
+		else
+		npx medusa db:migrate && yarn run build && npx medusa develop --verbose -H ::0 -p 9000
+		fi
+
 		#Start development environment
 		#npm run start   
